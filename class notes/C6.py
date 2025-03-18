b456ba2ca6ab7b0f0bd5e7d7398c487dcc5f03dd
Hector Jesus Lopez Ordaz <hectorjl@lcg.unam.mx>
2025-03-18 12:29:13 -0600

    Add Python script for nucleotide manipulation and counting

---
+++ class notes/C6.py
@@ -0,0 +1,21 @@
+nitrogen_bases = ['A', 'T', 'C', 'G']
+
+print(nitrogen_bases[1])
+
+nitrogen_bases[1] = 'U'
+print(nitrogen_bases[1])
+
+nucleotides = ["A", "T", "G", "C", "G", "T", "A", "G", "C"]
+
+print(nucleotides[0:5])
+
+print(nucleotides[-3:])
+
+print(nucleotides[::-1])
+
+no_format = list("ATGCTTCGA")
+repetitions = []
+repetitions.append = no_format.count("A") 
+repetitions.append = no_format.count("T")
+repetitions.append = no_format.count("G")
+repetitions.append = no_format.count("C")
